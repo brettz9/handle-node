5b201392635d80786b2ce20b046c93476f8f749f
Brett Zamir <brettz9@yahoo.com>
2015-01-30 11:29:02 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # handle-node
-Simple callback delegator based on DOM Node type
+
+Simple callback delegator based on DOM Node type.
+
+Provides an alternative to switches and numeric constant-based delegation.
+Useful for human-readable node traversal which does not necessarily
+require going through the whole tree as with DOM `NodeIterator`s or `TreeWalker`s.
+
+## Installation
+
+Node:
+
+```
+npm install handle-node
+```
+
+```js
+var handleNode = require('handle-node');
+```
+
+Browser:
+
+```html
+<script src="handle-node/index.js"></script>
+```
+
+## Usage
+
+Supply a node followed by a handler object whose all optional properties
+(human-readable Node type names) should be set to a callback which will
+be passed the supplied Node. The return value will be the result of
+invoking the callback which corresponds to the supplied node's type.
+
+Here is a demonstration reimplementing `textContent`:
+
+```js
+var textContent = handleNode(node, {
+    element: function (node) {
+        return Array.from(node.childNodes).reduce(function (str, node) {
+            return str + handleNode(node);
+        }, '');
+    },
+    text: function (node) {
+        return node.nodeValue;
+    }
+});
+```
+
+Other arguments can also be passed in after `node` and before the
+handler object, and these will also be supplied to the callbacks:
+
+```js
+var textContent = handleNode(node, arg1, arg2, {
+    element: function (node, arg1, arg2) {
+        return handleNode(node, arg1, arg2);
+    },
+    text: function (node, arg1, arg2) {
+        return node.nodeValue;
+    }
+});
+```
+
+## API
+
+The handler object can take the following optional properties:
+
+- element
+- attribute
+- text
+- cdata
+- entityReference
+- entity
+- processingInstruction
+- comment
+- document
+- documentType
+- documentFragment
+- notation
---
+++ index.js
@@ -0,0 +1,53 @@
+/*jslint vars:true, node:true*/
+var module;
+(function (undef) {'use strict';
+
+/**
+* Returns the value from executing a callback on a supplied callback object according to the type of node supplied
+* @param {Node} node An XML or HTML node
+* @param {object} var_args Callback object whose properties--all optional (element, attribute, text, cdata, entityReference, entity, processingInstruction, comment, document, documentType, documentFragment, notation) are callbacks which will be passed
+the supplied arguments (besides the callback object)
+* @returns {any} The result of calling the relevant callback
+*/
+function handleNode (node) {
+    var args = Array.from(arguments).slice(0, -1);
+    var cbObj = arguments[arguments.length - 1];
+
+    switch (node.nodeType) {
+    case 1: // Element
+        return cbObj.element && cbObj.element.apply(cbObj, args);
+    case 2: // Attribute node
+        return cbObj.attribute && cbObj.attribute.apply(cbObj, args);
+    case 3: // Text
+        return cbObj.text && cbObj.text.apply(cbObj, args);
+    case 4: // CDATA
+        return cbObj.cdata && cbObj.cdata.apply(cbObj, args);
+    case 5: // Entity reference
+        return cbObj.entityReference && cbObj.entityReference.apply(cbObj, args);
+    case 6: // Entity
+        return cbObj.entity && cbObj.entity.apply(cbObj, args);
+    case 7: // Processing instruction
+        return cbObj.processingInstruction && cbObj.processingInstruction.apply(cbObj, args);
+    case 8: // Comment
+        return cbObj.comment && cbObj.comment.apply(cbObj, args);
+    case 9: // Document
+        return cbObj.document && cbObj.document.apply(cbObj, args);
+    case 10: // Document type
+        return cbObj.documentType && cbObj.documentType.apply(cbObj, args);
+    case 11: // Document fragment
+        return cbObj.documentFragment && cbObj.documentFragment.apply(cbObj, args);
+    case 12: // Notation
+        return cbObj.notation && cbObj.notation.apply(cbObj, args);
+    }
+}
+
+// EXPORTS
+
+if (module) {
+    module.exports = handleNode;
+}
+else {
+    window.handleNode = handleNode;
+}
+
+}());
